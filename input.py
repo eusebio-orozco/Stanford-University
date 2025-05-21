48d3a5b931a9c926892e17f71fa7b683e87cc480
Eusebio Orozco <35247790+eusebio-orozco@users.noreply.github.com>
2025-05-21 11:22:41 -0600

    week 3

---
+++ input.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+input
+variable = input("dato a ingresar: ")
+
+"""
+print("test...")
+variable = input("cual es tu nombre? ")
+print(f"{variable} ,es tu nombre")
---
+++ print.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+SCRIPT ESCRITO EN LA TERMINAL DE GNU/LINUX
+"""
+
+print("hola")
+variable = "eusebio"
+print(f"hola de nuevo {variable}")
+
+"""
+variable = eusebio
+print(f"variable")
+"""
